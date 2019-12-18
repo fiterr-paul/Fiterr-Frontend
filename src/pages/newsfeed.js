64b4d4a77fd8caf0ba3d19ee816783c62c261afa
Jordan Fasso <jordan.fasso@gmail.com>
2019-12-18 16:52:56 +1100

    post components, context initialised

--- src/pages/newsfeed.js
+++ src/pages/newsfeed.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import NewPost from '../components/posts/NewPost'
 
 const newsfeed = () => {
+    getPosts() // post context method to get posts of people user follows
+
     return (
         <>
            <section className="body">
                 <div className="container">
                     <h1>Newsfeed</h1>
+                    <div>
+                        <NewPost />
+                    </div>
                 </div>
             </section> 
         </>
