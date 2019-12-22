06eee1eab85a775fe66dfc6788871d1bb33b20fc
Jordan Fasso <jordan.fasso@gmail.com>
2019-12-23 09:45:21 +1100

    user posts returned, displayed on profile page

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,13 +1,16 @@
 import React, {useState, useContext, useEffect } from 'react'
 import ProfileContext from '../../context/profile/profileContext'
 import AuthContext from '../../context/auth/authContext'
+import PostContext from '../../context/post/postContext'
 
 const Profile = (props) => {
     const profileContext = useContext(ProfileContext);
     const authContext = useContext(AuthContext)
+    const postContext = useContext(PostContext)
     // destructure anything from profileContext
     const { profile, getProfile } = profileContext
     const { user, isAuthenticated, loadUser } = authContext
+    const { myPosts } = postContext
     const defaultPic = "https://image.shutterstock.com/z/stock-vector-man-avatar-profile-picture-vector-icon-153720509.jpg"
     if(!isAuthenticated){loadUser()}
     if(!profile){getProfile()}
@@ -29,6 +32,19 @@ const Profile = (props) => {
                     return <p>{follower.username}</p>
                 })}
                 <h3>Following: {profile.following.length}</h3>
+            </div>
+
+            <div className="user-posts">
+                {myPosts.map(post, index=> {
+                    return(
+                        <div key={index}>
+                            <h4 className="postTitle">{post.title}</h4>
+                            <p className="content">{post.content}</p>
+                            <p className="likes">{post.likes.length()}</p>
+                            <p className="postDate">{post.postedAt}</p>
+                        </div>
+                    )
+                })}
             </div>
         </>
     )
